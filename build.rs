04a041d43bcf411cb4e66ba1cf96792161f1a3a1
Sam Goldman <sgoldman216@gmail.com>
2020-12-19 21:05:22 -0700

    Update build for submodule

--- build.rs
+++ build.rs
@@ -3,9 +3,9 @@ use flatc_rust;
 use std::path::Path;
 
 fn main() {
-    println!("cargo:rerun-if-changed=src/chess.fbs");
+    println!("cargo:rerun-if-changed=chess_flat_buffer/chess.fbs");
     flatc_rust::run(flatc_rust::Args {
-        inputs: &[Path::new("src/chess.fbs")],
+        inputs: &[Path::new("chess_flat_buffer/chess.fbs")],
         out_dir: Path::new("target/flatbuffers/"),
         ..Default::default()
     }).expect("flatc");
