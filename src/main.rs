c3a73e0f5c6933a5dbef0cbe784d705b4d97eca1
Sam Goldman <sgoldman216@gmail.com>
2020-12-20 18:30:31 -0700

    Add parallelization

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::fs::File;
 use glob::glob;
 use std::collections::{HashMap};
 use regex::Regex;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 #[allow(non_snake_case)]
 #[path = "../target/flatbuffers/chess_generated.rs"]
@@ -35,7 +37,7 @@ fn bin_year(game: crate::chess_flatbuffers::chess::Game) -> String {
 }
 
 fn bin_month(game: crate::chess_flatbuffers::chess::Game) -> String {
-    game.month().to_string()
+    format!("{:02}", game.month())
 }
 
 fn main() -> io::Result<()> {
@@ -62,23 +64,16 @@ fn main() -> io::Result<()> {
             .required(true)
             .multiple(true)
             .min_values(1))
+        .arg(Arg::with_name("num_threads")
+            .long("num_threads")
+            .takes_value(true)
+            .default_value("6"))
         .get_matches();
 
-    let mut db = Database {
+    let db = Arc::new(Mutex::new(Database {
         children: HashMap::new(),
         data: vec![]
-    };
-
-    let mut available_filters: HashMap<&str, FilterFn> = hashmap![];
-
-    let filter_factories = vec![
-        (Regex::new(r#"minGameElo(\d+)"#).unwrap(), min_game_elo_filter_factory as FilterFactoryFn),
-        (Regex::new(r#"maxGameElo(\d+)"#).unwrap(), max_game_elo_filter_factory),
-        (Regex::new(r#"year(\d+)"#).unwrap(), year_filter_factory),
-        (Regex::new(r#"month(\d+)"#).unwrap(), month_filter_factory),
-        (Regex::new(r#"day(\d+)"#).unwrap(), day_filter_factory),
-        (Regex::new(r#"minMoves(\d+)"#).unwrap(), min_moves_filter_factory)
-    ];
+    }));
 
     let mut available_statitistcs: HashMap<&str, Statistic> = hashmap![
         "gameCount" => ("gameCount".to_string(), map_count as MapFn, fold_sum as FoldFn),
@@ -92,31 +87,7 @@ fn main() -> io::Result<()> {
         "captureMin" => ("captureMin".to_string(), map_num_captures, fold_min)
     ];
 
-    let mut selected_filters = vec![];
-
     let selected_bins = vec![bin_year as BinFn, bin_month];
-
-    match matches.values_of("filters") {
-        Some(filter_strs) => {
-            for filter_str in filter_strs {
-                for i in 0..filter_factories.len() {
-                    let filter_factory = &filter_factories[i];
-        
-                    for cap in filter_factory.0.captures_iter(filter_str) {
-                        let value = cap[1].parse::<i32>().unwrap();
-                        let filter = filter_factory.1(value);
-                        selected_filters.push(filter);
-                    }
-                }
-        
-                match available_filters.remove(filter_str) {
-                    Some(v) => selected_filters.push(v),
-                    None => {}
-                }
-            }
-        }
-        None => {}
-    };
     
     let mut selected_statistics = vec![];
 
@@ -127,45 +98,115 @@ fn main() -> io::Result<()> {
         }
     }
 
+
+    let filter_factories = vec![
+        (Regex::new(r#"minGameElo(\d+)"#).unwrap(), min_game_elo_filter_factory as FilterFactoryFn),
+        (Regex::new(r#"maxGameElo(\d+)"#).unwrap(), max_game_elo_filter_factory),
+        (Regex::new(r#"year(\d+)"#).unwrap(), year_filter_factory),
+        (Regex::new(r#"month(\d+)"#).unwrap(), month_filter_factory),
+        (Regex::new(r#"day(\d+)"#).unwrap(), day_filter_factory),
+        (Regex::new(r#"minMoves(\d+)"#).unwrap(), min_moves_filter_factory)
+    ];
+
     let file_glob = matches.value_of("glob").unwrap();
 
-    for entry in glob(file_glob).expect("Failed to read glob pattern") {
-        let file_name = entry.unwrap();
-        let mut file = File::open(file_name)?;
-
-        let mut data = Vec::new();
-        file.read_to_end(&mut data)?;
+    let entries = Arc::new(Mutex::new(glob(file_glob).expect("Failed to read glob pattern")));
     
-        let games = root_as_game_list(&data).unwrap().games().unwrap().iter();
+    let num_threads: i32 = matches.value_of("num_threads").unwrap().parse::<i32>().unwrap();
 
-        let filtered_games = games.filter(|game| {
-            for filter in &selected_filters {
-                if false == filter(*game) {
-                    return false;
+    let mut handles = vec![];
+
+    for _ in 0..num_threads {
+        let db = Arc::clone(&db);
+        let entries = Arc::clone(&entries);
+        let selected_statistics = selected_statistics.clone();
+        let selected_bins = selected_bins.clone();
+        let matches = matches.clone();
+        let filter_factories = filter_factories.clone();
+        let handle = thread::spawn(move || -> io::Result<()> {
+            let mut selected_filters = vec![];
+
+            let mut available_filters: HashMap<&str, FilterFn> = hashmap![];
+
+            match matches.values_of("filters") {
+                Some(filter_strs) => {
+                    for filter_str in filter_strs {
+                        for i in 0..filter_factories.len() {
+                            let filter_factory = &filter_factories[i];
+                
+                            for cap in filter_factory.0.captures_iter(filter_str) {
+                                let value = cap[1].parse::<i32>().unwrap();
+                                let filter = filter_factory.1(value);
+                                selected_filters.push(filter);
+                            }
+                        }
+                
+                        match available_filters.remove(filter_str) {
+                            Some(v) => selected_filters.push(v),
+                            None => {}
+                        }
+                    }
+                }
+                None => {}
+            };
+
+
+            loop {
+                let entry;
+                {
+                    let mut entries = entries.lock().unwrap();
+                    match entries.next() {
+                        Some(x) => entry = x,
+                        None => return Ok(())
+                    }
+                }
+
+                let file_name = entry.unwrap();
+                let mut file = File::open(file_name)?;
+        
+                let mut data = Vec::new();
+                file.read_to_end(&mut data)?;
+            
+                let games = root_as_game_list(&data).unwrap().games().unwrap().iter();
+        
+                let filtered_games = games.filter(|game| {
+                    for filter in &selected_filters {
+                        if false == filter(*game) {
+                            return false;
+                        }
+                    }
+                    true
+                });
+                
+                for game in filtered_games {
+                    for i in 0..selected_statistics.len() {
+                        let stat = &selected_statistics[i];
+                        let mut path = vec![stat.0.clone()];
+        
+                        for bin in &selected_bins {
+                            let new_bin = bin(game);
+                            path.push(new_bin);
+                        }
+        
+                        let mut db = db.lock().unwrap();
+                        
+                        let node = db.insert_path(path);
+                        node.data.push(stat.1(game));
+                    }
                 }
             }
-            true
         });
+        handles.push(handle);
+    }
 
-        for game in filtered_games {
-            for i in 0..selected_statistics.len() {
-                let stat = &selected_statistics[i];
-                let mut path = vec![stat.0.clone()];
-
-                for bin in &selected_bins {
-                    let new_bin = bin(game);
-                    path.push(new_bin);
-                }
-
-                let node = db.insert_path(path);
-                node.data.push(stat.1(game));
-            }
-        }
+    for handle in handles {
+        handle.join().unwrap()?;
     }
 
     for i in 0..selected_statistics.len() {
 
         let selected = &selected_statistics[i];
+        let mut db = db.lock().unwrap();
 
         let stat_node = db.insert_path(vec![selected.0.to_string()]);
 
