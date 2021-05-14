d51e552cccd2820886430ccff617a244ee954d4a
Sam Goldman <sgoldman216@gmail.com>
2021-05-13 19:59:05 -0600

    Migrate test UT to tests/ to get accurate coverage

--- src/basic_types/game_result.rs
+++ src/basic_types/game_result.rs
@@ -17,30 +17,3 @@ impl GameResult {
         }
     }
 }
-
-#[cfg(test)]
-mod tests_convert_from_u8 {
-    use super::*;
-
-    macro_rules! conversion_tests {
-        ($($name:ident: $value:expr,)*) => {
-        $(
-            #[test]
-            fn $name() {
-                let (input, expected) = $value;
-                assert_eq!(expected, GameResult::from_u8(input));
-            }
-        )*
-        }
-    }
-
-    conversion_tests! {
-        convert_0: (0, Some(GameResult::White)),
-        convert_1: (1, Some(GameResult::Black)),
-        convert_2: (2, Some(GameResult::Draw)),
-        convert_255: (255, Some(GameResult::Star)),
-        convert_45: (45, None),
-        convert_85: (85, None),
-        convert_125: (125, None),
-    }
-}
---
+++ tests/ut/test_game_result.rs
@@ -0,0 +1,24 @@
+use chess_analytics::basic_types::game_result::GameResult;
+
+#[test]
+fn test_from_u8() {
+
+    macro_rules! cases_from_u8 {
+        ($($name:literal: $value:expr,)*) => {
+        $(
+            let (input, expected) = $value;
+            assert_eq!(expected, GameResult::from_u8(input), $name);
+        )*
+        }
+    }
+
+    cases_from_u8! {
+        "convert_0": (0, Some(GameResult::White)),
+        "convert_1": (1, Some(GameResult::Black)),
+        "convert_2": (2, Some(GameResult::Draw)),
+        "convert_255": (255, Some(GameResult::Star)),
+        "convert_45": (45, None),
+        "convert_85": (85, None),
+        "convert_125": (125, None),
+    }
+}
