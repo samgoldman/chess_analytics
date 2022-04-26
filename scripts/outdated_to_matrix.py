97de53a1058895e0eed957dfc47f26736aced44f
Sam Goldman <6364482+samgoldman@users.noreply.github.com>
2022-04-25 20:57:26 -0600

    Debug, remove fields

--- scripts/outdated_to_matrix.py
+++ scripts/outdated_to_matrix.py
@@ -2,5 +2,7 @@ import json
 
 f = open('outdated.json')
 o = json.load(f)
-print(f"::set-output name=matrix::{o['dependencies']}")
+d = o['dependencies']
+
+print(f"::set-output name=matrix::{[{'name': x['name'], 'project': x['project'], 'latest': x['latest']} for x in d]}")
 f.close()
