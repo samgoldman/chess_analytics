93e0a0c390f9007d323c934b85bba0508d67cd3a
Sam Goldman <6364482+samgoldman@users.noreply.github.com>
2022-09-19 13:40:20 -0600

    Checkout entire history for bench job

--- scripts/bench_diff.sh
+++ scripts/bench_diff.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-BASE=$1
-HEAD=$2
-
 git checkout $1
 cargo criterion
 git checkout $2
 cargo criterion --message-format=json | tee bench_results.log
-git checkout $1
 
-./scripts/check_for_regressions bench_results.log
+python ./scripts/check_for_regressions.py bench_results.log
